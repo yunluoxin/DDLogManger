f88446b54373e6a186ff9555c470daffad21cf65
ZhangXiaodong <Xiaodong@zhangxingdeimac.lan>
2016-12-09 20:26:25 +0800

    Initial Commit

---
+++ DDLogManger/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DDLogManger
+//
+//  Created by ZhangXiaodong on 16/12/9.
+//  Copyright © 2016年 DDLogManager. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
